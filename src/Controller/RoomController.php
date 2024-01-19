0c7483efc47212baf2b9c21c384d2495cf36112b
Alexandre Viegas <alexandre_viegas@icloud.com>
2024-01-19 16:27:00 +0100

    ✨ Add Room controller

---
+++ src/Controller/RoomController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\RoomRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/r')]
+class RoomController extends AbstractController
+{
+    #[Route('/', name: 'app_room')]
+    public function index(
+        RoomRepository $roomRepository
+    ): Response {
+        return $this->render('room/index.html.twig', [
+            'rooms' => $roomRepository->findAll(),
+        ]);
+    }
+}
