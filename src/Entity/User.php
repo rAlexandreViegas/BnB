2674bcd55730fbfb4cff7b27430f3fc92eedf473
Alexandre Viegas <alexandre_viegas@icloud.com>
2024-01-19 12:21:35 +0100

    💫 Update User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -68,11 +68,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'boolean')]
     private $isVerified = false;
 
+    #[ORM\ManyToMany(targetEntity: Room::class)]
+    private Collection $favorites;
+
     public function __construct()
     {
         $this->rooms = new ArrayCollection();
         $this->reviews = new ArrayCollection();
         $this->bookings = new ArrayCollection();
+        $this->favorites = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -339,6 +343,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setIsVerified(bool $isVerified): static
     {
         $this->isVerified = $isVerified;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Room>
+     */
+    public function getFavorites(): Collection
+    {
+        return $this->favorites;
+    }
+
+    public function addFavorite(Room $favorite): static
+    {
+        if (!$this->favorites->contains($favorite)) {
+            $this->favorites->add($favorite);
+        }
+
+        return $this;
+    }
+
+    public function removeFavorite(Room $favorite): static
+    {
+        $this->favorites->removeElement($favorite);
 
         return $this;
     }
