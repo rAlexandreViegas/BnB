0cec0c16345c4333e032aacff173e209157760f1
Alexandre Viegas <alexandre_viegas@icloud.com>
2024-01-24 10:23:52 +0100

    💫 Update RegistrationController.php

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -86,6 +86,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('paris');
+        return $this->redirectToRoute('account');
     }
 }
