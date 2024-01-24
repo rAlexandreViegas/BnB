017bbda04831d1f94d4d10067bcf9445f2dd83bf
Alexandre Viegas <alexandre_viegas@icloud.com>
2024-01-24 16:22:32 +0100

    💄 Update Dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,9 +6,11 @@ use App\Entity\Room;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -37,14 +39,29 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('BnB');
+            ->setTitle('<img src="/images/icon.svg" width="50">')
+            ->setFaviconPath('/images/icon.svg')
+            ->renderContentMaximized();
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::section('Users');
         yield MenuItem::linkToCrud('Users', 'fas fa-list', User::class);
+        yield MenuItem::section('Assets');
         yield MenuItem::linkToCrud('Rooms', 'fas fa-list', Room::class);
+        yield MenuItem::linkToRoute('Back to app', 'fa fa-home', 'app_room');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user)
+            ->setName($user->getFullName())
+            ->setAvatarUrl('/uploads/users/' . $user->getImage())
+            ->addMenuItems([
+                MenuItem::linkToRoute('My Profile', 'fa fa-id-card', 'account', ['id' => $user->getId()]),
+            ]);
     }
 }
